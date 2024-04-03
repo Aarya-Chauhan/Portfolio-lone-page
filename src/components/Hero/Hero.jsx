0ba1b6cf4b4ad391640b1e990b7c98b814d0ae03
Aarya Chauhan <aaryachauhan11@gmail.com>
2024-04-03 21:31:59 +0530

    completed HEro section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,13 +4,17 @@ import { getImageUrl } from '../../utils';
 
 const Hero = () => {
   return (
-    <section>
-        <div className="">
-            <h1>Hi I'm Aarya</h1>
-            <p>I'm an aspiring Fullstack developer and Machine Learning enthusiast. Reach out if you like to learn more!</p>
-            <a href="mailto:aaryachauhan11@gmail.com">Contact me!!</a>
+    <section className={styles.container}>
+        <div className={styles.content}>
+            <h1 className={styles.title}>Hi I'm Aarya</h1>
+            <p className={styles.description}> I'm an aspiring Fullstack developer and Machine Learning enthusiast. Reach out if you like to learn more!</p>
+            <a className={styles.contactBtn}href="mailto:aaryachauhan11@gmail.com">Contact me!!</a>
         </div>
-        <img src={getImageUrl("hero/heroImage.png")} alt="Hero image of me " />
+        <img className={styles.heroImg} src={getImageUrl("hero/heroImage.png")} alt="Hero image of me " />
+        <div className={styles.topBlur}>
+        </div>
+        <div className={styles.bottomBlur}>
+        </div>
         
     </section>
     
