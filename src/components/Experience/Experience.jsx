42776dad1766b14c3b239f0c1ee372ed1dc97355
Aarya Chauhan <aaryachauhan11@gmail.com>
2024-04-04 21:46:51 +0530

    experience boilerplate

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './Experience.module.css';
+import { getImageUrl } from '../../utils';
+
+const Experience = () => {
+  return (
+    <section id='experience' className={styles.container}>
+        <div>
+            <div></div>
+            <ul></ul>
+        </div>
+
+    </section>
+  )
+}
+
+export default Experience
