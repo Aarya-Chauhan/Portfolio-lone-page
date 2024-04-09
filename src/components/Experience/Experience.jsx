34477c13c8defe4dcec88613747dfd5f14230b58
Aarya Chauhan <aaryachauhan11@gmail.com>
2024-04-09 20:39:44 +0530

    completion of experience section template

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,22 +1,38 @@
 import React from 'react'
 import styles from './Experience.module.css';
 import skill from "../../data/skills.json";
+import history from "../../data/history.json";
 import { getImageUrl } from '../../utils';
 
 const Experience = () => {
   return (
     <section id='experience' className={styles.container}>
-        <h2>Experience</h2>
-        <div>
-          <div>{
+        <h2 className={styles.title}>Experience</h2>
+        <div className={styles.content}>
+          <div className={styles.skills}>{
             skill.map((skill, id)=>{
-              return <div key={id}>
-                <div><img src={getImageUrl(skill.imageSrc)} alt={(skill.title)} /></div>
+              return <div key={id}className={styles.skill}>
+                <div className={styles.skillImageContainer}><img  src={getImageUrl(skill.imageSrc)} alt={(skill.title)} /></div>
                 <p>{skill.title}</p>
               </div>
             })
             }</div>
-          <ul></ul>
+          <ul className={styles.history}>
+            {
+              history.map((historyItem, id)=>{
+                return <li key={id} className={styles.historyItem}>
+                  <img src={getImageUrl(historyItem.imageSrc)} alt={`${historyItem.organisation} logo`} />
+                  <div className={styles.historyItemDetails}>
+                    <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
+                    <p>{`${historyItem.startDate}- ${historyItem.endDate}`}</p>
+                    <ul>{historyItem.experiences.map((experience, id)=>{
+                      return <li key={id}>{experience}</li>
+                    })}</ul>
+                  </div>
+                </li>
+              })
+            }
+          </ul>
         </div>
 
     </section>
