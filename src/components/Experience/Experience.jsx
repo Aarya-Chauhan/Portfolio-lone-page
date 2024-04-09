3e7e01d61ad26c521fb4ac21443f9bdddf57e7b1
Aarya Chauhan <aaryachauhan11@gmail.com>
2024-04-09 20:06:08 +0530

    adding assets

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import styles from './Experience.module.css';
+import skill from "../../data/skills.json";
 import { getImageUrl } from '../../utils';
 
 const Experience = () => {
   return (
     <section id='experience' className={styles.container}>
+        <h2>Experience</h2>
         <div>
-            <div></div>
-            <ul></ul>
+          <div>{
+            skill.map((skill, id)=>{
+              return <div key={id}>
+                <div><img src={getImageUrl(skill.imageSrc)} alt={(skill.title)} /></div>
+                <p>{skill.title}</p>
+              </div>
+            })
+            }</div>
+          <ul></ul>
         </div>
 
     </section>
