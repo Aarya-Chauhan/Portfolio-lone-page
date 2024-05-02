e25deec5905d465deb79c388891f736cc50e8482
Aarya Chauhan <aaryachauhan11@gmail.com>
2024-05-02 20:29:56 +0530

    final updates before hosting

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -14,7 +14,7 @@ const Skills = () => {
             return (
               <div key={id} className={styles.skill}>
                 <div className={styles.skillImageContainer}>
-                  <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+                  <img className={styles.image} src={getImageUrl(skill.imageSrc)} alt={skill.title} />
                 </div>
                 <p>{skill.title}</p>
               </div>
